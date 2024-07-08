3c2cb6b559e741458824ef151393cfd7891dc963
Seonghun <77530278+kawai10@users.noreply.github.com>
2024-07-08 14:19:12 +0900

    Add - 로컬 도메인인 seonghun.com을 ALLOWED_HOSTS에 추가, 스태틱 파일 경로 추가

--- config/settings/base.py
+++ config/settings/base.py
@@ -188,6 +188,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, STATIC_URL)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -197,3 +198,5 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = "users.User"
 
 INTERNAL_IPS = ["localhost", "127.0.0.1"]
+
+ALLOWED_HOSTS = ["seonghun.com"]
