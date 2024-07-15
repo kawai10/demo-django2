063fa7f018136b7c911f0f6ca5734841e86e5e8b
Seonghun <77530278+kawai10@users.noreply.github.com>
2024-07-15 14:13:55 +0900

    Fix - github action에서는 postgres를 로컬 ubuntu에 설치하기 때문에 기본값으로 따로 연동해야함.

--- config/settings/production.py
+++ config/settings/production.py
@@ -7,11 +7,11 @@ ALLOWED_HOSTS = ["seonghun.com"]
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": env("POSTGRES_DB"),
-        "USER": env("POSTGRES_USER"),
+        "NAME": env("POSTGRES_DB", default="postgres"),
+        "USER": env("POSTGRES_USER", default="postgres"),
         "PASSWORD": env("POSTGRES_PASSWORD"),
-        "HOST": env("POSTGRES_HOST"),
-        "PORT": env("POSTGRES_PORT"),
+        "HOST": env("POSTGRES_HOST", default="localhost"),
+        "PORT": env("POSTGRES_PORT", default=5432),
     }
 }
 
